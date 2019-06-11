b98bf6036692802dde4423bced915bb6d1686f64
Mo Fan <46451461+fanmok@users.noreply.github.com>
2019-06-11 23:56:29 +0800

    Update update.php

--- update.php
+++ update.php
@@ -8,14 +8,11 @@ $os= $_GET['plus'];
 $imei= $_GET['imei'];   
 $rsp = array('status' => 0);  
 if (isset($appid) && isset($version)) {  
-if($version !== "1.3"){ //最新版本号  
-$rsp['status'] = 5;  
+if($version !== "1.4"){ //最新版本号  
+$rsp['status'] = 6;  
 $rsp['title'] = "版本更新";  
-$rsp['note'] = "version 1.3";  
-if($os == 'Android'){  
-$rsp['url'] = "https://www.szchenxi.cf/app/app.html"; //安卓安装包地址  
-}else{  
-$rsp['url'] = "https://www.szchenxi.cf/app/app.html"; //苹果点击更新后跳转的地址，可以是自己的网页的，也可以是苹果商店的  
-}}}exit(json_encode($rsp));}  
+$rsp['note'] = "version 1.4";  
+$rsp['url'] = "https://www.szchenxi.cf/app/app.html";
+}}exit(json_encode($rsp));}  
 $data=down($getdh);    
 echo $data;  
