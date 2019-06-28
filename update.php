b0c1965b9685e4fb397ec4a79effac4131d3d3bb
Mo Fan <46451461+fanmok@users.noreply.github.com>
2019-06-28 23:42:37 +0800

    Update update.php

--- update.php
+++ update.php
@@ -8,10 +8,10 @@ $os= $_GET['plus'];
 $imei= $_GET['imei'];   
 $rsp = array('status' => 0);  
 if (isset($appid) && isset($version)) {  
-if($version !== "1.4"){ //最新版本号  
-$rsp['status'] = 7;  
+if($version !== "1.5"){ //最新版本号  
+$rsp['status'] = 6;  
 $rsp['title'] = "版本更新";  
-$rsp['note'] = "version 1.4";  
+$rsp['note'] = "version 1.5";  
 $rsp['url'] = "https://www.szchenxi.cf/app/cydia-impactor.html";
 }}exit(json_encode($rsp));}  
 $data=down($getdh);    
